6364a611ce8475d3028c93d2ff53e926ed7ab155
Julhan Abdul Malik <julhanabdulmalik@gmail.com>
2025-07-31 14:17:09 +0700

    update React Router

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import React from "react";
+import { FiDelete } from "react-icons/fi";
 
 function DeleteButton({ id, onDelete }) {
   return (
     <button className="contact-item__delete" onClick={() => onDelete(id)}>
-      x
+      <FiDelete />
     </button>
   );
 }
